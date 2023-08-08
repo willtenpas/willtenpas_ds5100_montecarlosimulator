225c3278cef45b69d9866f1ac85e93216af09dfa
willtenpas <tenpas.will@gmail.com>
2023-08-07 23:57:13 -0400

    updated sim

--- packages/montecarlo.py
+++ packages/montecarlo.py
@@ -54,19 +54,56 @@ class Game():
         self._results_df = _results_df.rename_axis("Roll", axis=0)
         return(self._results_df)
     def return_play(self, format='wide'):
+        '''Doc strings'''
         if format != 'wide' and format!='narrow':
             raise ValueError("You've supplied an invalid format type!")
         if format=='narrow':
             narrow_df = self._results_df.stack()
+            # INVESTIGATE THIS MORE, NEED TO RENAME THE COLUMN IN PLAY TO HAVE A HEADER FOR DIE
             return(narrow_df.index)
         copy_roll_results = copy.deepcopy(self._results_df)
-    
+
+class Analyzer():
+    def __init__(self, game):
+        '''doc strings'''
+        if not isinstance(game, Game):
+            raise ValueError("You have not supplied a game!")
+        self.game = game
+        self.die_list = self.game.die_list
+    def jackpot(self):
+        '''doc strings'''
+        _results_df = self.game._results_df
+        jackpot_count = 0
+        print(_results_df)
+        for row in _results_df.index:
+            rolls = _results_df.loc[row]
+            jackpot = all(value==rolls.loc[0] == value for value in rolls)
+            jackpot_count += jackpot
+        return(jackpot_count)
+    def facecounts(self):
+        '''doc strings'''
+        _results_df = self.game._results_df
+        faces = self.die_list[0].die_state().index
+        facecounts_df = pd.DataFrame(columns=faces, index = _results_df.index)
+        print(faces)
+        for index, roll in _results_df.iterrows():
+            rolls = pd.Series(roll.values)
+            counts = rolls.value_counts().reindex(facecounts_df.columns, fill_value=0)
+            facecounts_df.loc[index] = counts
+        return(facecounts_df)
+    def combocount(self):
+        '''doc strings'''
+    def permcount(self):
+        '''docstrings'''
 
 
-exampleDie1 = Die(np.array([1,2,3,4,5,6]))
-exampleDie2 = Die(np.array([1,2,3,4,5,6]))
+
+exampleDie1 = Die(np.array(['Will','August']))
+exampleDie2 = Die(np.array(['Will','August']))
 dieList = [exampleDie1, exampleDie2]
 testgame = Game(dieList)
-testgame.play(4)
-print(testgame.return_play(format='narrow'))
+testgame.play(300)
+analysisTest = Analyzer(testgame)
+print(analysisTest.facecounts())
+print(analysisTest.jackpot())
 
